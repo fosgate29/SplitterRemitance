47cd0c286677adf9f19d84546026ce6ccf14e4f5
fosgate29 <luizhamilton29@gmail.com>
2019-05-23 09:31:02 -0300

    fix: updated remittance test

---
+++ test/remittance.js
@@ -0,0 +1,35 @@
+const { constants } = require('openzeppelin-test-helpers');
+const { ZERO_ADDRESS } = constants;
+
+const Remittance = artifacts.require("./Remittance.sol");
+
+const Promise = require("bluebird");
+const { shouldFail } = require("openzeppelin-test-helpers");
+
+web3.eth = Promise.promisifyAll(web3.eth);
+
+contract('Remittance', function(accounts) {
+  
+  let contract;
+
+  const owner = accounts[0]; 
+
+  const contribution = web3.utils.toWei(web3.utils.toBN(1), 'ether');
+  
+  const bob   = accounts[2];   
+  const carol = accounts[3];
+
+  const someUser = accounts[4];
+
+  beforeEach(async () => {
+    contract = await Remittance.new(1000, {from:owner})
+  });
+
+  it("should be owned by owner", async () => {
+    const _owner = await contract.owner({from:owner})
+    assert.strictEqual(_owner, owner, "Contract is not owned by owner");
+  });
+
+  
+
+});
