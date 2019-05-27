2285878cc006dc6b3cb9e3709598117dd2102298
fosgate29 <luizhamilton29@gmail.com>
2019-05-27 10:53:06 -0300

    Added solim and fixed solium errors

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,66 +2,66 @@ pragma solidity ^0.5.0;
 
 
 /**
- * @title Splitter contract. 
+ * @title Splitter contract.
  * Consensys Dev Academy exercise - Small projects - Module 4
- * 
+ *
  */
 contract Splitter {
-    
+
     //states
     address payable public owner;
-    
+
     event LogTransferred(address indexed receiver, uint amount);
-    event LogSplitterCreated(address indexed msgSender, address indexed user1, address indexed user2, 
-                              uint half , uint  remainder);
-    
+    event LogSplitterCreated(address indexed msgSender, address indexed user1, address indexed user2,
+    uint half , uint  remainder);
+
     mapping(address => uint) public balances;
-    
+
     //Constructor
     constructor() public {
         owner = msg.sender;
     }
-    
+
     //split the value received between two users and update their balance
-    function split(address address1, address address2) payable public returns(bool success) {
+    function split(address address1, address address2) public payable returns(bool success) {
         //checking address not equal to zero to make sure balances mapping has no address equal to zero
-        require(address1 != address(0) && address2 != address(0) , 'Invalid address for address1 or address2');
-        require(msg.value > 0 , 'Split must receive a value');
-        
-        balances[address1] +=  msg.value / 2;
-        balances[address2] +=  msg.value / 2;
-        
+        require(address1 != address(0) && address2 != address(0), "Invalid address for address1 or address2");
+        require(msg.value > 0, "Split must receive a value");
+
+        balances[address1] += msg.value / 2;
+        balances[address2] += msg.value / 2;
+
         //msg sender gets 1 wei if it is an odd number
         if(msg.value % 2 == 1){
             balances[msg.sender] += 1;
         }
-        
-        emit LogSplitterCreated(msg.sender, address1, address2,  msg.value / 2 , msg.value % 2);
-        
+
+        emit LogSplitterCreated(msg.sender, address1, address2,  msg.value / 2, msg.value % 2);
+
         return true;
     }
-   
-    // user collect his money 
-    function withdrawFunds() public returns(bool success) { 
+
+    // user collect his money
+    function withdrawFunds() public returns(bool success) {
         uint amountToTransfer = balances[msg.sender];
-        
-        require(amountToTransfer>0, 'Amount to transfer must be greater than zero');  //thanks @nikhilwins
-        
-        balances[ msg.sender] = 0;
+
+        require(amountToTransfer>0, "Amount to transfer must be greater than zero");  //thanks @nikhilwins
+
+        balances[msg.sender] = 0;
         msg.sender.transfer(amountToTransfer);
-            
+
         emit LogTransferred(msg.sender, amountToTransfer);
-        
+
         return true;
     }
-    
-    //It will kill the contract and return all remain funds to the 
+
+    //It will kill the contract and return all remain funds to the
     //contract owner
     function killMe() public {
-        require(msg.sender == owner, 'Sender must be the owner');
-        
+        require(msg.sender == owner, "Sender must be the owner");
+
         uint amount = address(this).balance;
-        
+
         emit LogTransferred(msg.sender, amount);
 
         selfdestruct(owner);
